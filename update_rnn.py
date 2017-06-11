ee36233c1159127b097cc7f2274a780b7ea6dfb1
tjoe <dongfang3654@outlook.com>
2017-06-11 22:03:53 +0800

    原始rnn

--- update_rnn.py
+++ update_rnn.py
@@ -40,34 +40,75 @@ def feature_change(train):
     batch_features = []
     train = train.reshape(batch_size, 15, 4, 101, 101)
     for s, sample in enumerate(train):
-
         # 逐层提取特征
         times = []
         for t in range(15):
-            heights = []
+            time = []
+            ring = []
+            centre = []
+            local = []
+            map_variance = []
+            bit_map = []
+            frequency = []
             for h in range(4):
-                # 1.提取最后一个时序的中心范围总/平均反射率，有四层4*50*2==400
-                height = []
-                for k in range(0, 51):
-                    height.append(np.sum(sample[t,h, k:101 - k, k:101 - k]))
-                    height.append(np.average(sample[t,h, k:101 - k, k:101 - k]))
-                # 2.提取局部区域的平均反射率10*10*4
-                for row in range(0, 101, local_step):
-                    if row == 100:
-                        break
-                    for col in range(0, 101, local_step):
-                        if col == 100:
-                            break
-                        height.append(np.average(sample[t,h, row:row + local_step, col:col + local_step]))
-                #提取不同反射率范围的个数
-                height.append(np.sum(sample[t,h,:,:] <=20))
-                height.append(np.sum((sample[t, h, :, :] > 20) & (sample[t, h, :, :] <= 40)))
-                height.append(np.sum((sample[t, h, :, :] > 40) & (sample[t, h, :, :] <= 60)))
-                height.append(np.sum((sample[t, h, :, :] > 60) & (sample[t, h, :, :] <= 80)))
-                height.append(np.sum(sample[t, h, :, :] > 80))
+                # 1.提取环平均反射率，有四层4*50*2==400
+                sum_set = []
+                for t in range(15):
+                    # 51圈中心总量
+                    each_time_sum = []
+                    for k in range(0, 51, centre_step):
+                        each_time_sum.append(np.sum(sample[t, h, k:101 - k, k:101 - k]))
+                    sum_set.append(each_time_sum)
+                # 提取一环环的平均值：25环
+                ring_set = []
+                for t in range(15):
+                    each_time_ring = []
+                    for a in range(0, 50, 2):
+                        each_time_ring.append(
+                            (sum_set[t][a] - sum_set[t][a + 2]) / ((101 - 2 * a) ** 2 - (101 - 2 * a - 4) ** 2))  # 25个环
+                    ring_set.append(each_time_ring)
+                    # 15个时间点，4层高度，15*4*25=1500 特征
+                    ring.extend(each_time_ring)
+                #2.提取中心范围
+                centre.append(np.average(sample[t, h, 45:56, 45:56]))
+                centre.append(np.average(sample[t, h, 40:61, 40:61]))
+                centre.append(np.average(sample[t,h]))
+                # 各点数据的方差
+                map_variance.append(np.var(sample[t, h, 45:56, 45:56]))
+                map_variance.append(np.var(sample[t, h, 40:61, 40:61]))
 
-                heights.append(height)
-            times.append(heights)
+                #局部区域特征
+                for step in [10,20, 50, 100]:
+                    local_average = []
+                    for row in range(0, 101, step):
+                        if row == 100:
+                            break
+                        for col in range(0, 101, step):
+                            if col == 100:
+                                break
+                            local_average.append(np.average(sample[t,h, row:row + step, col:col + step]))
+                            local.append(np.average(sample[t,h, row:row + step, col:col + step]))
+                    local.append(np.var(local_average))
+
+                #加入中间20*20个点
+                bit_map.extend(sample[t,h, 40:61, 40:61].ravel())
+                #计入频数
+                frequency.append(np.sum(sample[t,h] <= 20))
+                frequency.append(np.sum((sample[t,h] > 20) & (sample[t,h] <= 40)))
+                frequency.append(np.sum((sample[t,h] > 40) & (sample[t,h] <= 60)))
+                frequency.append(np.sum((sample[t,h] > 60) & (sample[t,h] <= 80)))
+                frequency.append(np.sum((sample[t,h] > 80) & (sample[t,h] <= 100)))
+                frequency.append(np.sum((sample[t,h] > 100) & (sample[t,h] <= 120)))
+                frequency.append(np.sum((sample[t,h] > 120) & (sample[t,h] <= 140)))
+                frequency.append(np.sum((sample[t,h] > 140) & (sample[t,h] <= 160)))
+                frequency.append(np.sum((sample[t,h] > 160)))
+            time.extend(ring)
+            time.extend(centre)
+            time.extend(local)
+            time.extend(map_variance)
+            time.extend(bit_map)
+            time.extend(frequency)
+            times.append(time)
         batch_features.append(times)
     train = np.array(batch_features)
     return train
@@ -76,13 +117,13 @@ def feature_change(train):
 
 # Parameters
 learning_rate = 0.001
-training_iters = 10                #外循环次数
+training_iters = 50                #外循环次数
 batch_size = 500
 minibatch_size = 100                #批量梯度下降批大小
-repeat_round = 200                  #shuffle 次数
+repeat_round = 1                  #shuffle 次数
 
 # Network Parameters
-n_input = 5+400+102# MNIST data input (img shape: 28*28)
+n_input = 3856# MNIST data input (img shape: 28*28)
 n_steps = 15 # timesteps
 n_hidden = 128 # hidden layer num of features
 
@@ -91,41 +132,20 @@ centre_step = 1
 local_step = 5
 
 # tf Graph input
-x1 = tf.placeholder("float", [None, n_steps, n_input])
-x2 = tf.placeholder("float", [None, n_steps, n_input])
-x3 = tf.placeholder("float", [None, n_steps, n_input])
-x4 = tf.placeholder("float", [None, n_steps, n_input])
+x = tf.placeholder("float", [None, n_steps, n_input])
+
 
 y = tf.placeholder("float", [None,1])
 
 #define weight
-weights1 = {
+weights = {
     "out":tf.Variable(tf.random_normal([n_hidden,1]))
 }
-biases1 = {
-    "out": tf.Variable(tf.random_normal([1]))
-}
-weights2 = {
-    "out":tf.Variable(tf.random_normal([n_hidden,1]))
-}
-biases2 = {
-    "out": tf.Variable(tf.random_normal([1]))
-}
-weights3 = {
-    "out":tf.Variable(tf.random_normal([n_hidden,1]))
-}
-biases3 = {
-    "out": tf.Variable(tf.random_normal([1]))
-}
-weights4 = {
-    "out":tf.Variable(tf.random_normal([n_hidden,1]))
-}
-biases4 = {
+biases = {
     "out": tf.Variable(tf.random_normal([1]))
 }
 
-
-def RNN1(x, weights, biases):
+def RNN(x, weights, biases):
 
     # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
     x = tf.unstack(x, n_steps, 1)
@@ -142,64 +162,11 @@ def RNN1(x, weights, biases):
         return pred
 
 
-def RNN2(x, weights, biases):
-    # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.unstack(x, n_steps, 1)
-
-    # Define a lstm cell with tensorflow
-    with tf.variable_scope("RNN",reuse=True):
-        lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-        # Get lstm cell output
-        outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
-
-        # Linear activation, using rnn inner loop last output
-        pred = tf.matmul(outputs[-1], weights['out']) + biases['out']
-        return pred
 
 
-def RNN3(x, weights, biases):
-    # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.unstack(x, n_steps, 1)
-
-    # Define a lstm cell with tensorflow
-    with tf.variable_scope("RNN",reuse=True):
-        lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-        # Get lstm cell output
-        outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
-
-        # Linear activation, using rnn inner loop last output
-        pred = tf.matmul(outputs[-1], weights['out']) + biases['out']
-        return pred
+pred = RNN(x, weights, biases)
 
 
-def RNN4(x, weights, biases):
-    # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.unstack(x, n_steps, 1)
-
-    # Define a lstm cell with tensorflow
-    with tf.variable_scope("RNN",reuse=True):
-        lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-        # Get lstm cell output
-        outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
-
-        # Linear activation, using rnn inner loop last output
-        pred = tf.matmul(outputs[-1], weights['out']) + biases['out']
-        return pred
-
-# 最後一層
-weight_final_out = tf.Variable(tf.random_normal([4,1]))
-biases_final_out = tf.Variable(tf.random_normal([1]))
-
-pred1 = RNN1(x1, weights1, biases1)
-pred2 = RNN2(x2, weights2, biases2)
-pred3 = RNN3(x3, weights3, biases3)
-pred4 = RNN4(x4, weights4, biases4)
-
-rnn_integrate = tf.concat([pred1, pred2, pred3, pred4],1)
-pred = tf.add(tf.matmul(rnn_integrate,weight_final_out ), biases_final_out)
 
 
 #define loss and optimizer
@@ -212,15 +179,27 @@ init = tf.global_variables_initializer()
 train_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\train_shuffle.txt"
 test_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_testA\\data_new\\CIKM2017_testA\\testA.txt"
 
-result_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\results\\rnn_result{}.csv"
-model_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\models\\rnn_model{}.ckpt"
+result_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\rnn_results\\rnn_result{}.csv"
+model_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\rnn_models\\rnn_model{}.ckpt"
 
+save_train_path = "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\new_features\\feature_rnn_train.npy"
+save_test_path =  "F:\\data\\tianchi\\CIKM2017\\CIKM2017_train\\data_new\\CIKM2017_train\\new_features\\feature_rnn_test.npy"
 #linux
 # train_path = "../data/train.txt"
 # test_path = "../data/test.txt"
 # result_path = "./results/rnn_result{}.csv"
 # model_path= "./models/rnn_model{}.ckpt"
 
+train_iterator = read_data_sets(train_path, batch_size)
+train = []
+for k,batch_train in train_iterator:
+    if k == 2:
+        break
+    train.append(feature_change(batch_train))
+train = np.array(train).reshpae(10000,15,-1)
+print(train.shape)
+with open(save_train_path,"wb") as f:
+    np.save(f,train)
 
 with tf.Session() as sess:
     sess.run(init)
@@ -232,12 +211,9 @@ with tf.Session() as sess:
 
             if k == 19:
                 train = feature_change(train)
-                train = train.reshape((batch_size, n_steps, 4,n_input))
-                train = tf.unstack(train, 4, 2)
-                train = sess.run(train)
-                score = sess.run(cost, feed_dict={x1:train[0], x2:train[1],x3:train[2],x4:train[3],y:lables})
+                train = train.reshape((batch_size, n_steps,n_input))
+                score = sess.run(cost, feed_dict={x:train,y:lables})
                 print("final 500 sample score = {}".format(score))
-                break
 
             start = clock()
             #sample_size个样本进行shuffle,批量进行训练，每次minibatch_size个样本
@@ -252,27 +228,20 @@ with tf.Session() as sess:
                 train, lables = train[prem], lables[prem]
                 for z in range(0,batch_size, minibatch_size):
                     minibatch_train, minibatch_lables = train[z:z+minibatch_size], lables[z:z+minibatch_size]
-                    minibatch_train = minibatch_train.reshape((minibatch_size, n_steps,4, n_input))
+                    minibatch_train = minibatch_train.reshape((minibatch_size, n_steps, n_input))
                     #分四层
-                    minibatch_train = tf.unstack(minibatch_train,4,2)
-                    minibatch_train = sess.run(minibatch_train)
-
-                    sess.run(optimizer, feed_dict={x1:minibatch_train[0], x2:minibatch_train[1],x3:minibatch_train[2],x4:minibatch_train[3],y:minibatch_lables})
-            train = train.reshape((batch_size, n_steps,4, n_input))
-            train = tf.unstack(train, 4, 2)
-            train = sess.run(train)
-            score = sess.run(cost, feed_dict={x1:train[0], x2:train[1],x3:train[2],x4:train[3],y:lables})
+                    sess.run(optimizer, feed_dict={x:minibatch_train,y:minibatch_lables})
+            train = train.reshape((batch_size, n_steps,n_input))
+            score = sess.run(cost, feed_dict={x:train,y:lables})
             print("training_iters:{}/{}  round:{}/{} socre={} time={:.2f}s".format(i, training_iters,k,10000/batch_size, score, clock()-start))
         #每次外循环跑一次测试数据
-        learning_rate *= 0.9
+        # learning_rate *= 0.9
         result = []
         test_iterator = read_data_sets(test_path, batch_size)
         for m ,(minibatch_test, test_lables) in enumerate(test_iterator):
             minibatch_test = feature_change(minibatch_test)
-            minibatch_test = minibatch_test.reshape((batch_size, n_steps,4, n_input))
-            minibatch_test = tf.unstack(minibatch_test, 4, 2)
-            minibatch_test = sess.run(minibatch_test)
-            temp = sess.run(pred,feed_dict={x1: minibatch_test[0], x2: minibatch_test[1],x3: minibatch_test[2],x4: minibatch_test[3]})
+            minibatch_test = minibatch_test.reshape((batch_size, n_steps,n_input))
+            temp = sess.run(pred,feed_dict={x: minibatch_test})
             result.append(temp)
         result = np.array(result).reshape(2000,-1)
         np.savetxt(result_path.format(i), result,delimiter=',')
